4ab4d5d3f515162b40054212f8407febc4a08626
heidiklumpe <heidi.klumpe@gmail.com>
2023-03-13 11:04:32 -0400

    Updates to scripts for generating training sets to accept WORKERS as an argument.

--- scripts/cluster_training_eval_sets.py
+++ scripts/cluster_training_eval_sets.py
@@ -41,6 +41,22 @@ def params_change(params):
     # Return path to json file:
     return foldername+"parameters.json"
 
+#%% Submit single script
+cell_class = 'CcaSR_gillespie_simple_noE'
+WORKERS = 0 #8, script interprets 0 as None
+
+job_id = qsub.submit(
+    dcc_repo_path + "scripts/get_training_eval_sets.py",
+    conda_env="delta_env",
+    args = [cell_class, WORKERS, ],
+    hardware_requirements = dict(
+        time_limit = 1, #2
+        cores=8, #4
+        gpus=1,
+        mem_per_core=4,
+        )
+    )
+
 #%% Submit scripts
 
 cell_class = 'CcaSR_gillespie'
@@ -57,10 +73,11 @@ for new_params in new_params_list:
     new_params_dir = params_change(new_params)
     
     # Submit qsub request for single job:
+    # ARGS: cell_class (str), workers (int, default None), new_params_dir (str, optional)
     job_id = qsub.submit(
         dcc_repo_path + "scripts/get_training_eval_sets.py",
         conda_env="delta_env",
-        args = [cell_class, new_params_dir,],
+        args = [cell_class, None, new_params_dir,],
         hardware_requirements = dict(
             time_limit = 16, #2
             cores=2, #4
