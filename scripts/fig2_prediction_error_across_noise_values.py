039858a6ac64aded830d56c47537c083de36267c
heidiklumpe <heidi.klumpe@gmail.com>
2023-03-13 10:50:08 -0400

    Update figures for comparison across different models.

--- scripts/fig2_prediction_error_across_noise_values.py
+++ scripts/fig2_prediction_error_across_noise_values.py
@@ -68,8 +68,9 @@ def get_params(simul_id):
 
 #%% Get information about simulations
 
-simul_dir_list = glob.glob(dcc_repo_path + '/assets/models/2023-03-10*')
-                    
+simul_dir_list = glob.glob(dcc_repo_path + '/assets/models/2023-03-0[789]*') + \
+                 glob.glob(dcc_repo_path + '/assets/models/2023-03-1*')
+                 
 simul_id_list = [simul_dir.split('/')[-1] for simul_dir in simul_dir_list]
 horizon_list = []
 training_folder_list = []
@@ -90,8 +91,13 @@ for simul_id in simul_id_list:
     cell_class_list += [training_params['cell_class']]
     epochs_list += [training_params['training_parameters']['epochs']]
     datasets_list += [training_params['datasets_folder']]
-    h1_list += [model_params['h1']]
-    h2_list += [model_params['h2']]
+    if 'h1' in model_params.keys():
+        h1_list += [model_params['h1']]
+    else:
+        h1_list += [np.nan]
+    if 'h2' in model_params.keys():
+        h2_list += [model_params['h2']]
+    else: h2_list += [np.nan]
     
 df_meta = pd.DataFrame({'simul_id': simul_id_list,
                         'horizon': horizon_list,
@@ -122,16 +128,22 @@ default_h2 = np.abs((df_meta['h2'] - _h2) / _h2) < 0.01
 default_h1h2 = np.abs(((df_meta['h1'] / df_meta['h2']) - _h1h2) / _h1h2) < 0.01
 
 #%% Plot error as function of speed (same steady state) 
-model_style_dict = {'CcaSR_gillespie_simple': '.',
+model_style_dict = {'CcaSR_gillespie_simple': 'o',
                     'CcaSR_gillespie': 's'}
+models_list = [key for key in model_style_dict.keys()]
 
+markersize=3
 simul_slice = default_training_size & default_past_steps & default_horizon & default_h1h2
 
 df_past = df_meta.loc[simul_slice].sort_values(by=['cell_class','h1','h2']).reset_index(drop=True)
 
-plt.figure(figsize=(8,3))
+fig, axes = plt.subplots(1,2, figsize=(8,3), sharey=True)
 for i in range(len(df_past)):
+    
+    
     cell_class = df_past.loc[i,'cell_class']
+    plt_idx = models_list.index(cell_class)
+    
     h1 = df_past.loc[i,'h1']
     h2 = df_past.loc[i,'h2']
     simul_id = df_past.loc[i, 'simul_id']
@@ -139,13 +151,89 @@ for i in range(len(df_past)):
     fluo, fluo_pred = get_fluo_and_pred(simul_id) 
     RMSE = np.median(np.sqrt((fluo - fluo_pred)**2), axis=0)
     t = [x/12. for x in range(len(RMSE))]
-    plt.plot(t, RMSE, 
+    axes[plt_idx].plot(t, RMSE, 
              model_style_dict[cell_class], 
-             label=f'{cell_class}, h1={h1:.3f}, h2={h2:.3f}')
+             markersize=markersize,
+             label=f'h1={h1:.2e}, h2={h2:.2e}')
 
-plt.legend(bbox_to_anchor=(1,1))
-plt.xlabel('time (h)')
-plt.ylabel(f'Median error\n{np.shape(fluo)[0]} cells')
+for i in range(2):
+    axes[i].set_xlabel('time (h)')
+    axes[i].set_title(models_list[i])
+    axes[i].legend()
+axes[0].set_ylabel(f'Median error\n{np.shape(fluo)[0]} cells')
 plt.tight_layout()
 plt.savefig(dcc_repo_path+'/assets/figures/effect_of_rate.png', dpi=600)
 
+#%% Plot error as function of steady-state (same dilution, different production) 
+model_style_dict = {'CcaSR_gillespie_simple': 'o',
+                    'CcaSR_gillespie': 's'}
+models_list = [key for key in model_style_dict.keys()]
+
+markersize=3
+simul_slice = default_training_size & default_past_steps & default_horizon & default_h2
+
+df_past = df_meta.loc[simul_slice].sort_values(by=['cell_class','h1','h2']).reset_index(drop=True)
+
+fig, axes = plt.subplots(1,2, figsize=(8,3), sharey=True)
+for i in range(len(df_past)):
+    
+    
+    cell_class = df_past.loc[i,'cell_class']
+    plt_idx = models_list.index(cell_class)
+    
+    h1 = df_past.loc[i,'h1']
+    h2 = df_past.loc[i,'h2']
+    simul_id = df_past.loc[i, 'simul_id']
+    
+    fluo, fluo_pred = get_fluo_and_pred(simul_id) 
+    RMSE = np.median(np.sqrt((fluo - fluo_pred)**2), axis=0)
+    t = [x/12. for x in range(len(RMSE))]
+    axes[plt_idx].plot(t, RMSE, 
+             model_style_dict[cell_class], 
+             markersize=markersize,
+             label=f'h1={h1:.2e}, h2={h2:.2e}')
+
+for i in range(2):
+    axes[i].set_xlabel('time (h)')
+    axes[i].set_title(models_list[i])
+    axes[i].legend()
+axes[0].set_ylabel(f'Median error\n{np.shape(fluo)[0]} cells')
+plt.tight_layout()
+plt.savefig(dcc_repo_path+'/assets/figures/effect_of_h1.png', dpi=600)
+
+#%% Plot error as function of steady-state (different dilution, same production) 
+model_style_dict = {'CcaSR_gillespie_simple': 'o',
+                    'CcaSR_gillespie': 's'}
+models_list = [key for key in model_style_dict.keys()]
+
+markersize=3
+simul_slice = default_training_size & default_past_steps & default_horizon & default_h1
+
+df_past = df_meta.loc[simul_slice].sort_values(by=['cell_class','h1','h2']).reset_index(drop=True)
+
+fig, axes = plt.subplots(1,2, figsize=(8,3), sharey=True)
+for i in range(len(df_past)):
+    
+    
+    cell_class = df_past.loc[i,'cell_class']
+    plt_idx = models_list.index(cell_class)
+    
+    h1 = df_past.loc[i,'h1']
+    h2 = df_past.loc[i,'h2']
+    simul_id = df_past.loc[i, 'simul_id']
+    
+    fluo, fluo_pred = get_fluo_and_pred(simul_id) 
+    RMSE = np.median(np.sqrt((fluo - fluo_pred)**2), axis=0)
+    t = [x/12. for x in range(len(RMSE))]
+    axes[plt_idx].plot(t, RMSE, 
+             model_style_dict[cell_class], 
+             markersize=markersize,
+             label=f'h1={h1:.2e}, h2={h2:.2e}')
+
+for i in range(2):
+    axes[i].set_xlabel('time (h)')
+    axes[i].set_title(models_list[i])
+    axes[i].legend()
+axes[0].set_ylabel(f'Median error\n{np.shape(fluo)[0]} cells')
+plt.tight_layout()
+plt.savefig(dcc_repo_path+'/assets/figures/effect_of_h2.png', dpi=600)
