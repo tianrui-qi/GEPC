2a813ae2929462d73387d5799a85b86191a25b7c
Jean-Baptiste Lugagne <jb.lugagne@gmail.com>
2024-03-11 09:29:46 -0400

    Updated server script to run on SCC

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -9,32 +9,38 @@ import time
 import json
 import sys
 
-sys.path.append("D:/shared_packages/deepcellcontrol")
+sys.path.insert(0, "/projectnb/dunlop/JB/deepcellcontrol")
 import deepcellcontrol as dcc
 
-res_folder = "Y:/projectnb2/dunlop/JB/deepcellcontrol/assets/models/2022-05-07_21-52-16_f651d065-2d7b-4f55-8dee-10e48d6e79b2"
-log_folder = f"D:/onlineserverlogs/{time.strftime('%Y-%m-%d_%H-%M-%S')}/"
+models_folder = "/projectnb/dunlop/JB/deepcellcontrol/assets/models/"
+# model_folder = models_folder+"2022-05-07_21-52-16_f651d065-2d7b-4f55-8dee-10e48d6e79b2/model.hdf5"
+model_file = models_folder+"2024-01-30_17-36-02_4fc0cb07-7dda-4580-92fd-26550476275a/model_besteval.hdf5"
+log_folder = f"/projectnb/dunlop/JB/deepcellcontrol/assets/onlineserverlogs/{time.strftime('%Y-%m-%d_%H-%M-%S')}/"
 horizon = 24
 iterations = 25
 particles = 40
 block = True
 
+# Init controller:
 controller = dcc.control.SplitLSTMMPC(
-    model_file = res_folder + '/model.hdf5',
+    model_file = model_file,
     strategy_optimizer=dcc.control.BinaryParticleSwarmOptimizer(
         horizon=horizon, iterations=iterations, particles=particles
         )
     )
 
+# Init server:
 server = dcc.server.Server(controller, device = "GPU")
+server.batch_size = 10 # Just to be safe
 server.start()
 
-onlineserver = dcc.server.SocketServer(server, savelog=log_folder)
+# Init TCP/IP server:
+onlineserver = dcc.server.SocketServer(server, port= 7555, savelog=log_folder)
 onlineserver.start()
 
 with open(log_folder+"parameters.json", "w") as f:
     parameters = {
-        "trained_model_folder": res_folder,
+        "trained_model": model_file,
         "horizon": horizon,
         "iterations": iterations,
         "particles": particles
